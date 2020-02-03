676f1c4901850f32ca60822a84e6dc0d6e6f475a
fabioberger <me@fabioberger.com>
2020-02-03 11:34:31 +0100

    Add schema validation to request params/body of meta transaction endpoints

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,5 +1,9 @@
+import * as metaTransactionFillRequestSchema from './meta_transaction_fill_request_schema.json';
+import * as metaTransactionQuoteRequestSchema from './meta_transaction_quote_request_schema.json';
 import * as swapQuoteRequestSchema from './swap_quote_request_schema.json';
 
 export const schemas = {
     swapQuoteRequestSchema,
+    metaTransactionFillRequestSchema,
+    metaTransactionQuoteRequestSchema,
 };
