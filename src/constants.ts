3c506575c3aaea46fe099fbefa611ec92b1772cc
fabioberger <me@fabioberger.com>
2020-02-12 17:20:29 +0100

    Add initial implementation of meta txn fee inclusion in meta transaction quotes

--- src/constants.ts
+++ src/constants.ts
@@ -14,6 +14,7 @@ export const DEFAULT_LOGGER_INCLUDE_TIMESTAMP = true;
 export const ONE_SECOND_MS = 1000;
 export const ONE_MINUTE_MS = ONE_SECOND_MS * 60;
 export const TEN_MINUTES_MS = ONE_MINUTE_MS * 10;
+export const ERC20_BRIDGE_ASSET_PREFIX = '0xdc1600f3';
 
 // Swap Quoter
 export const QUOTE_ORDER_EXPIRATION_BUFFER_MS = ONE_SECOND_MS * 30; // Ignore orders that expire in 30 seconds
